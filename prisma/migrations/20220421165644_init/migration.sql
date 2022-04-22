85ea16f6a677e23180f0dd6dd2af339f5764954e
franzwollang <franzwollang@gmail.com>
2022-04-22 09:49:32 +0200

    Partial implementation of emoji reactions

---
+++ prisma/migrations/20220421165644_init/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "EmojiComment" ALTER COLUMN "number" SET DEFAULT 0;
+
+-- AlterTable
+ALTER TABLE "EmojiPost" ALTER COLUMN "number" SET DEFAULT 0;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Prisma, PrismaClient } from '@prisma/client'
 import express from 'express'
+import { connect } from 'http2'
 
 const prisma = new PrismaClient()
 const app = express()
@@ -7,7 +8,7 @@ const app = express()
 app.use(express.json())
 
 app.post(`/post/`, async (req, res) => {
-    const { author, email, title, content, image, tags, reactions } = req.body
+    const { author, email, title, content, image, tags } = req.body
 
     const result = await prisma.post.create({
         data: {
@@ -16,8 +17,7 @@ app.post(`/post/`, async (req, res) => {
             title,
             content, 
             image,
-            tags,
-            reactions
+            tags
         },
     })
 
@@ -26,7 +26,9 @@ app.post(`/post/`, async (req, res) => {
 
 app.get('/post/', async (req, res) => {
 
-    const posts = await prisma.post.findMany({})
+    const posts = await prisma.post.findMany({
+
+    })
 
     res.json(posts)
 })
@@ -48,6 +50,39 @@ app.put('/post/:id/views', async (req, res) => {
     } catch (error) {
         res.json({ error: `Post with ID ${id} does not exist in the database` })
     }
+})
+
+app.put(`/post/:post_id/reactions/`, async (req, res) => {
+    const { post_id } = req.params;
+    const { reactions } = req.body
+
+    const result = await prisma.post.update({
+        where: {
+            id: Number(post_id)
+        },
+        data: {
+            reactions: { 
+                connectOrCreate: reactions.map( (emojiObj: {character: string, number: number}) => {
+                return { 
+                    where: { 
+                        emoji: { connect: { character: emojiObj.character } },
+                        number: { increment: 1 }
+                    },
+                    create: { 
+                        postId: Number(post_id),
+                        emoji: {
+                            connectOrCreate: {
+                                where: { character: emojiObj.character },
+                                create: { character: emojiObj.character }
+                            }
+                        }
+                    },
+                }
+            })}
+        },
+    })
+
+    res.json(result)
 })
 
 app.post(`/post/:post_id/comment/`, async (req, res) => {
